8d3ed2372ba473bf9a50e59026e4b57aa42c1a40
Adminixtrator <sadiqheritage927@gmail.com>
2020-01-02 02:06:11 +0000

    Tidying up

--- ims/imscan/views.py
+++ ims/imscan/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 import requests
 import pytesseract as pt
 from PIL import Image
@@ -24,6 +25,19 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def index(request):
+    
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file'] 
+        if allowed_file(file.name):
+            try:
+                extracted_text = ocr_core(file)
+                if extracted_text != '':
+                    extracted_text=extracted_text
+                else:
+                    extracted_text='Could not identify text in image.. \nPlease check image and try again..'
+            except:
+                extracted_text='Error occured! Please check your file and try again'
+        return render(request, 'index.html', {'extracted_text': extracted_text})
     
     return render(request, 'index.html')
 
